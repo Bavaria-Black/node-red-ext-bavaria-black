17879751b87ff2ab81ebaf11cf119aa58d4d3bcd
Christian Dirnhofer <christian@dirnhofer.net>
2020-09-24 18:51:46 +0200

    added utils.map function for mapping numeric values to a specified range

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 var ui = require('./ui.js');
 var validators = require('./validators.js');
 var observer = require('./observer.js');
+var utils = require('./utils.js');
 
 module.exports = {
-    validators : validators,
-    ui : ui,
+    validators: validators,
+    ui: ui,
     observer: observer,
+    utils: utils,
 };
